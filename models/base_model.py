87ec4e09dad97993729cab566264bc15f38cdc68
root <root@DESKTOP-TPQ0LCA.localdomain>
2022-10-26 12:17:27 +0300

    BaseClass Initialization

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import uuid
+from datetime import datetime 
+'''
+BaseModel Module for all common attributes
+'''
+
+
+class BaseModel():
+    '''
+    Base Model Class
+    '''
+
+    def __init__(self):
+	''' Initialization of BaseModel
+	    public instance attributes: 
+		id(str): uniqued identifier
+		created_at(DT): instance creation date time
+		updated_at(DT): instance change date time
+	'''
+        id = str(uuid.uuid4())
+        created_at = datetime.isoformat(datetime.today())
+	updated_at = datetime.isoformat(datetime.today())
+
+    def __str__(self):
+ 	''' Print informal representation of an instance
+	'''
+	pass
+	
