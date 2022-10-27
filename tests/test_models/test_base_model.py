94c4ed5181304e52dafc30f013b0668fb0e81810
Paul-Bett <bettpaul6@gmail.com>
2022-10-27 10:43:56 +0300

    base model unittest setup

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,26 +6,34 @@ import sys
 '''
     
 
-class base_module_TestCase(unittest.TestCase):
+class base_model_TestCase(unittest.TestCase):
+    ''' Test for base model class
+    '''
     def setUp(self):
         ''' Initialize testing framework 
             This will automatically call for all test we run
         '''
-        self.my_model = BaseModel()
+        self.my_instance = BaseModel()
 
     def tearDown(self):
         ''' Tidies up agter the test method has been run
             Will run wether test method succeeds or not
         '''
-        del self.my_model 
+        del self.my_instance 
 
     def test_id_set(self):
         ''' Test if auto-generation of ID is successful
         '''
-        self.assertTrue(self.my_model.id,"ID not generated")
+        self.assertTrue(self.my_instance.id,"ID not generated")
 
-    def test_create_time(self):
+    def test_create_time_set(self):
         ''' Test if create_time was generated
         '''
-        self.assertTrue(self.my_model.id, "Create_Time not generated")
+        self.assertTrue(self.my_instance.id, "Create_Time not generated")
 
+    def test_id_type(self):
+        '''
+            Checks that the type of the id is string.
+        '''
+        self.assertEqual("<class 'str'>", str(type(self.my_instance.id)))
+    
