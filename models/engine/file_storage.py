1668690ffcc2098dbe52baf24b5d9f87ff7abab6
Paul-Bett <bettpaul6@gmail.com>
2022-10-27 16:12:32 +0300

    reload check if file exist included

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 import models
+import os
 
 '''Module: file_storage
     Class: FileStorage
@@ -47,6 +48,3 @@ class FileStorage():
         if os.path.isfile(__file_path) and os.access(__file_path, os.R_OK):
             with open(FileStorage.__file_path, encoding="UTF8") as jsonfile:
                 self.__objects = json.load(jsonfile)
-
-
-
