a2384919858be66a2b8c8705fe0136d3167b26db
Paul-Bett <bettpaul6@gmail.com>
2022-11-07 10:18:43 +0300

    state,city,amenity,place,review classes created

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import shlex
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 ''' Console for AirBnB Clone Project
 '''
